bbca874884c46bb4e832216cd77a70b3ec6da455
dhruv <dhruv.g123@gmail.com>
2021-02-15 15:04:03 -0500

    added interceptors and internationalization

--- src/main/java/com/pluralsight/conference/ConferenceConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceConfig.java
@@ -1,12 +1,45 @@
 package com.pluralsight.conference;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class ConferenceConfig {
+public class ConferenceConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/files/**")
+				.addResourceLocations("/WEB-INF/pdf/");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localChangeInterceptor());
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.US);
+		return slr;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
 
 	@Bean
 	public ViewResolver viewResolver() {
